48a7b6a3879b27d71dd79744eeb050487ced07de
firebird11 <hbgassel@gmail.com>
2017-01-01 16:52:59 +0100

    Revert "g3: add initial quickcircle app"
    This reverts commit 2a20f4d00fec23d2f9ad71a6d64f27dd7a3effee.

--- g3.mk
+++ g3.mk
@@ -114,10 +114,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:system/etc/sec_config
 
-# QuickCircle Case App
-PRODUCT_PACKAGES += \
-    QuickCircle
-
 # Keystore
 PRODUCT_PACKAGES += \
     keystore.msm8974
