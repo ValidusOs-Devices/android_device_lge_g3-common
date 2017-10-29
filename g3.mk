205ed889dec53a057c8d2f32b3bc05f5be6e5819
ZjemCiKolege <frager1911@gmail.com>
2017-10-29 14:43:32 +0100

    add configstore
    Change-Id: I0c4327e869869b27305887a40fb42c413e875d8b

--- g3.mk
+++ g3.mk
@@ -103,6 +103,10 @@ PRODUCT_PACKAGES += \
     Snap \
     camera.msm8974
 
+# Configstore
+PRODUCT_PACKAGES += \
+    android.hardware.configstore@1.0-service
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@2.0-impl \
